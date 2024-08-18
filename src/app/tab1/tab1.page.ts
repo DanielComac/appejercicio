693e70fad08fe137a225375b800aca58507ea6f9
UrielGon03 <uriel@advantedigital.com>
2024-08-17 21:25:16 -0600

    cambios visuales

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -10,12 +10,49 @@ import { PhotoService } from '../servicios/photo.service';
 })
 export class Tab1Page implements OnInit {
   routines: any[] = [];
+  //const routine = {
+  //  id: Date.now(), // Unique ID for the routine, based on the current timestamp
+  //  nombre: 'Full Body Workout', // Name of the routine
+  //  img: '', // Image URL for the routine
+  //  ejercicios: [
+  //    {
+  //      id: '1', // Unique ID for the exercise
+  //      nombre: 'Push-Ups', // Name of the exercise
+  //      descripcion: 'An exercise to strengthen the upper body.', // Description of the exercise
+  //      series: 3, // Number of series
+  //      repeticiones: 15, // Number of repetitions per series
+  //      img: 'https://via.placeholder.com/100' // Image URL for the exercise
+  //    },
+  //    {
+  //      id: '2', // Unique ID for the exercise
+  //      nombre: 'Squats', // Name of the exercise
+  //      descripcion: 'An exercise to strengthen the lower body.', // Description of the exercise
+  //      series: 4, // Number of series
+  //      repeticiones: 12, // Number of repetitions per series
+  //      img: 'https://via.placeholder.com/100' // Image URL for the exercise
+  //    },
+  //    {
+  //      id: '3', // Unique ID for the exercise
+  //      nombre: 'Plank', // Name of the exercise
+  //      descripcion: 'An exercise to strengthen the core.', // Description of the exercise
+  //      series: 3, // Number of series
+  //      repeticiones: 30, // Duration in seconds, treated as repetitions
+  //      img: 'https://via.placeholder.com/100' // Image URL for the exercise
+  //    }
+  //  ],
+  //  duracion: 45, // Total duration of the routine in minutes
+  //  descanso: 60, // Rest time between exercises in seconds
+  //  numdia: [1, 3, 5], // Days on which the routine is performed (e.g., Monday, Wednesday, Friday)
+  //  createdAt: '08/18/2024', // Creation date of the routine
+  //  createdby: 1 // ID of the user who created the routine
+  //};
   currDay = new Date().getDay()
 
   constructor(private photoService: PhotoService, private router: Router) {}
 
   ngOnInit() {
     this.routines = this.photoService.getRoutines()
+    //this.routines = [this.routine, this.routine, this.routine]1
   }
 
   get todayRoutines(): any[]{
