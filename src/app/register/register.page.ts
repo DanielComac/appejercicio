18006ed19121ed5464820a4939f6cfcbbb0d027d
DanielComac <danielcomacxbox@gmail.com>
2024-08-17 21:22:57 -0600

    adwaf

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,37 +1,31 @@
-// src/app/login/login.page.ts
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AlertController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
 })
-export class LoginPage {
+export class RegisterPage {
+  name: string = '';
   email: string = '';
   password: string = '';
 
   constructor(
-    private router: Router,
     private afAuth: AngularFireAuth,
+    private router: Router,
     private alertController: AlertController
   ) {}
 
-  async login() {
+  async register() {
     try {
-      const userCredential = await this.afAuth.signInWithEmailAndPassword(this.email, this.password);
-      if (userCredential.user) {
-        this.router.navigate(['/tabs']);
-      }
+      await this.afAuth.createUserWithEmailAndPassword(this.email, this.password);
+      this.router.navigate(['/login']);
     } catch (error) {
-      this.showAlert('Error', 'Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+      this.showAlert('Error', 'No se pudo registrar el usuario. Por favor, inténtalo de nuevo.');
     }
-  }
-
-  navigateToRegister() {
-    this.router.navigate(['/register']);
   }
 
   async showAlert(header: string, message: string) {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDlqENOqIMpMsBrg1rHF9waBM9lqXsphpk",
+    authDomain: "routinetracker2.firebaseapp.com",
+    projectId: "routinetracker2",
+    storageBucket: "routinetracker2.appspot.com",
+    messagingSenderId: "234081969809",
+    appId: "1:234081969809:web:0892bc733cea56cbb7c130",
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
